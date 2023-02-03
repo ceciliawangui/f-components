805c38ec25002ab8e681a1b1c26998a6aa6308df
git.assignment-1 <cesskui4034@gmail.com>
2023-02-03 03:15:57 +0300

    add existing project files to git

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Main.css"
 
 function Main() {
   return (
     <div>
-      <p>
+      <p classname = "par1">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae at est
         asperiores ipsa. Eius totam voluptates fugiat pariatur ab,
         <br />
